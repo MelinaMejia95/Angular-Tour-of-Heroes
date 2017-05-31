7780c761a45ac97a76f1d8dfb3cf8931d6503d39
MelinaMejia95 <melinamejia95@gmail.com>
2017-05-30 21:21:36 -0500

    Styles and Heroes images

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,16 +2,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let heroes = [
-      {id: 11, name: 'Wolverine'},
-      {id: 12, name: 'Hulk'},
-      {id: 13, name: 'Iron Man'},
-      {id: 14, name: 'Thor'},
-      {id: 15, name: 'Captain America'},
-      {id: 16, name: 'Spider Man'},
-      {id: 17, name: 'Doctor Strange'},
-      {id: 18, name: 'Black Panther'},
-      {id: 19, name: 'Storm'},
-      {id: 20, name: 'Professor X'}
+      {id: 11, name: 'Wolverine', img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/00/537bcb1133fd7.jpg'},
+      {id: 12, name: 'Hulk', img: 'https://i.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg'},
+      {id: 13, name: 'Iron Man', img: 'https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg'},
+      {id: 14, name: 'Thor', img: 'https://i.annihil.us/u/prod/marvel/i/mg/7/10/537bc71e9286f.jpg'},
+      {id: 15, name: 'Captain America', img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/80/537ba5b368b7d.jpg'},
+      {id: 16, name: 'Spider Man', img: 'https://i.annihil.us/u/prod/marvel/i/mg/6/60/538cd3628a05e.jpg'},
+      {id: 17, name: 'Doctor Strange', img: 'https://i.annihil.us/u/prod/marvel/i/mg/1/30/537ba85979532.jpg'},
+      {id: 18, name: 'Black Panther', img: 'https://i.annihil.us/u/prod/marvel/i/mg/9/03/537ba26276348.jpg'},
+      {id: 19, name: 'Storm', img: 'https://i.annihil.us/u/prod/marvel/i/mg/c/b0/537bc5f8a8df0.jpg'},
+      {id: 20, name: 'Quicksilver', img: 'https://i.annihil.us/u/prod/marvel/i/mg/b/e0/537bc02b0fee5.jpg'}
     ];
     return {heroes};
   }
